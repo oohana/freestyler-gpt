4b0106d5991d46401b88292aaefe21ff4cf73a7a
Oren Ohana <oren.ohana@gmail.com>
2023-05-04 19:38:20 -0700

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home: NextPage = () => {
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Head>
-        <title>Twitter Bio Generator</title>
+        <title>RAP GENERATOR</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
